965d111f8c22719411b716123ca32cea22c1f146
Jason Botzas-Coluni <jason.botzas.coluni@gmail.com>
2021-04-12 17:58:00 -0400

    Pushed changes

--- src/components/Feed/MakePost.js
+++ src/components/Feed/MakePost.js
@@ -1,25 +1,39 @@
-import React from 'react';
+/* eslint-disable no-restricted-globals */
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Button from '@material-ui/core/Button';
+import CardActions from '@material-ui/core/CardActions';
+import TextField from '@material-ui/core/TextField';
+import ChipInput from 'material-ui-chip-input';
+import React, { useState } from "react";
+import firebase from 'firebase';
+
+
+const db = firebase.firestore()
+
+
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
     maxWidth: 600,
     textAlign: 'left'
+  },
+  area : {
+      minWidth:'550px'
+  },
+  tf : {
+    minWidth:'300px'
+  },
+  tf2 : {
+    minWidth:'300px',
+    marginBottom:'15px'
+
   },
   avatar: {
     backgroundColor: red[500],
@@ -28,24 +42,70 @@ const useStyles = makeStyles((theme) => ({
 
 export default function MakePost() {
   const classes = useStyles();
+  const [tags, setTags] = useState([]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [imgUrl, setImgUrl] = useState("");
+  const [git, setGit] = useState("");
+
+  const addPost = () =>{
+    db.collection("posts").add({
+        title,
+        git,
+        desc,
+        imgUrl,
+        tags,
+        email: firebase.auth().currentUser.email,
+        uid : firebase.auth().currentUser.uid,
+        timestamp: firebase.firestore.Timestamp.now(),
+    }).then(() => location.reload()
+    )
+}
+
+
+
+  const addTag = (tagName) => {
+    const tagSet = new Set(tags);
+    tagSet.add(tagName);
+    setTags(Array.from(tagSet));
+  }
+
+  const removeTag = (tagName) => {
+    setTags(tags.filter(tag => tag !== tagName));
+  }
+
 
   return (
     <Card className={classes.root} >
       <CardHeader
-        // action={
-        //   <IconButton aria-label="settings">
-        //     <MoreVertIcon />
-        //   </IconButton>
-        // }
         title="Make a post!"
-        subheader="Showcase..."
       />
       <CardContent>
-        <Typography variant="body2" color="textSecondary" component="p">
-          This impressive paella is a perfect party dish and a fun meal to cook together with your
-          guests. Add 1 cup of frozen peas along with the mussels, if you like.
-        </Typography>
+      <TextField required  onChange={e => setTitle(e.target.value)} className={classes.tf2} label="Enter your post title" />
+      <TextareaAutosize
+      required 
+      onChange={e => setDesc(e.target.value)}
+      className = {classes.area}
+      rowsMin={4}
+      aria-label="maximum height"
+      placeholder="Buzzwords go here..."
+    />
+    <ChipInput
+        required 
+        fullWidth
+        placeholder='Type and press enter to add chips'
+        value={tags}
+        onAdd={(chip) => addTag(chip)}
+        onDelete={(chip) => removeTag(chip)}
+        />
+    <TextField  onChange={e => setImgUrl(e.target.value)} className={classes.tf} id="img" label="Enter your image URL" />
+    <TextField required onChange={e => setGit(e.target.value)} className={classes.tf} id="git" label="Enter your GitHub repo" />
       </CardContent>
+      <CardActions>
+        <Button disabled={!title || !desc || !git} onClick={()=> addPost()} size="small" color="primary">
+          Post
+        </Button>
+      </CardActions>
     </Card>
   );
 }
